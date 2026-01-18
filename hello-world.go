3fcc3a0f7ce7c7a9987066497d414ef849ba48c1
zhang-andrew <72960866+zhang-andrew@users.noreply.github.com>
2026-01-19 00:23:34 +1100

    hello world

---
+++ hello-world.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world");
+
+	fmt.Println("go run hello-world.go")
+	
+	fmt.Println("go build hello-world.go")
+
+	fmt.Println("./hello-world")
+};
